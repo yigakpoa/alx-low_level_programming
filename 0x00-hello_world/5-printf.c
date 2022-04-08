bbdc92aba8f9073b155148f9606f0b7d14ec2b5f
yigakpoa <lyigakpoa@gmail.com>
2022-04-08 10:07:55 -0700

    alx biko kwa

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
-Â#include <stdio.h>
+#include <stdio.h>
 /**
 * main - Entry point
-*My hello, printf task
-*Return: return 1 after program execution
+* My hello, printf task
+* Return: return 1 after program execution
 */
 int main(void)
 {
