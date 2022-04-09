580842abe45f2e6e87cb65d4f9e8c511a0e70f6e
yigakpoa <lyigakpoa@gmail.com>
2022-04-09 12:02:05 -0700

    doneeee

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,7 +16,7 @@ for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 {
 for (digit3 = digit2 +1; digit3 < 10; digit3++)
 {
-putchar((digit1 % 10) + '0");
+putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
 putchar((digit3 % 10) + '0');
 
