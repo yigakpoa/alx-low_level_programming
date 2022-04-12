944334eef9ab6365ff4d58fae8d1839058b72fe8
yigakpoa <lyigakpoa@gmail.com>
2022-04-12 14:17:57 -0700

    prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,7 +9,7 @@
 int print_sign(int n)
 {
 	if (n > 0)
-{
+
 	_putchar ('+');
 	return (1);
 }
@@ -20,10 +20,11 @@ else if (n == 0)
 	return (0);
 }
 
-else
+	else
 {
 	_putchar ('-')
 	return (-1);
 }
 
 }
+
