ea0a1773ba9d657458031fced0b72e31441ed339
yigakpoa <lyigakpoa@gmail.com>
2022-04-12 14:24:45 -0700

    prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,28 +3,27 @@
 /**
  * print_sign - return 0 letter not lowercase, 1 letter lowercase
  *
- * @n: the int to print
+ *@n: the int to print
  * Return: Always 0.
  */
 int print_sign(int n)
 {
 	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
 
-	_putchar ('+');
-	return (1);
-}
-
-else if (n == 0)
-{
-	_putchar ('0');
-	return (0);
-}
+	else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
 
 	else
-{
-	_putchar ('-')
-	return (-1);
-}
+	{
+		_putchar ('-');
+		return (-1);
+	}
 
 }
-
