803b477032dfdc5d81ffebaea7aeeb6993d3d16a
yigakpoa <lyigakpoa@gmail.com>
2022-04-27 03:55:39 -0700

    ysjdb

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,21 +1,18 @@
 #include "main.h"
-
 /**
- * print_numbers - Prints the number zero to nine
- *
- * Return: Always 0 (Success)
+ * print_numbers - print numbers
+ * Return: Always 0.
  */
-
 void print_numbers(void)
 {
-
 int i;
 
 i = 0;
-
-for (i = 0; i < 10; i++)
-
-_putchar(i + '0');
+while (i < 10)
+{
+_putchar (i + '0');
+i++;
+}
 
 _putchar ('\n');
 }
