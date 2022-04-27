71469c9f161d2b700c44b5d406734ffe687c5daf
yigakpoa <lyigakpoa@gmail.com>
2022-04-27 00:58:53 -0700

    yes task 3-7

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_numbers - Print the numbers since 0 up to 9
- *
- * Return: The numbers since 0 up to 9
- */
+  * print_numbers - Print the numbers since 0 up to 9
+  *
+  * Return: The numbers since 0 up to 9
+  */
 void print_numbers(void)
 {
 	int x;
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Print the numbers since 0 up to 9
- *
- *Description: Prints the numbers excluding 2 and 4
- *
- * Return: The numbers since 0 up to 9
- */
+  * print_most_numbers - Print the numbers since 0 up to 9
+  *
+  * Description: Prints the numbers excluding 2 and 4
+  *
+  * Return: The numbers since 0 up to 9
+  */
 void print_most_numbers(void)
 {
 	int x = 0;
